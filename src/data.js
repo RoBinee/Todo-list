aca278bf1cf955e1f9617bb0f56b288d07eb4794
Robinee <yb6925@naver.com>
2023-05-03 20:14:59 +0900

    Add basic structure of data and remove temporary data

--- src/data.js
+++ src/data.js
@@ -1,14 +1,30 @@
-const projects = ['project1', 'project2'];
-
-const todos = [
+const data = [
   {
-    title: 'todo1',
-    dueDate: '2023-05-02',
+    project: 'One project',
+    todos: [
+      {
+        title: 'One project todo',
+        dueDate: '2027-12-02',
+      },
+      {
+        title: 'One project todo2',
+        dueDate: '2023-06-03',
+      },
+    ],
   },
   {
-    title: 'todo2',
-    dueDate: '2023-06-03',
+    project: 'Two project',
+    todos: [
+      {
+        title: 'Two project todo',
+        dueDate: '2027-12-02',
+      },
+      {
+        title: 'Two project todo2',
+        dueDate: '2023-06-03',
+      },
+    ],
   },
 ];
 
-export { projects, todos };
+export { data };
