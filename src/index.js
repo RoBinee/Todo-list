f87150e35fdfc4fb5a50f8bc2b4ab8b851b02f7d
Robinee <yb6925@naver.com>
2023-05-03 20:55:54 +0900

    Write a code to execute form input

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import './style.css';
 import '@fortawesome/fontawesome-free/js/all';
 import { loadPage } from './loadPage.js';
+import { getElement } from './getElement.js';
 
 loadPage();
+
+const sidebarForm = getElement(undefined, 'project-form');
+
+sidebarForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formInput = getElement('.project-input');
+  const projectName = formInput.value;
+  if (projectName.length === 0) {
+    alert('Write down new project name');
+    return;
+  } else if (projectName.length > 0) {
+    //make element using input.value
+    const element = document.createElement('li');
+    element.classList.add('project');
+    element.innerHTML = `<h2 class="project-title">${projectName}</h2>
+      <button class="remove-btn"><i class="fa-solid fa-xmark"></i></button>`;
+    //append in projects-container
+    const container = getElement('.projects-container');
+    container.append(element);
+    // clean input value
+    formInput.value = '';
+  }
+});
