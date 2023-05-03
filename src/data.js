443b32cf28aa4e6032634d1bc55f379ca0a2c540
Robinee <yb6925@naver.com>
2023-05-03 19:53:20 +0900

    Add projects and todos

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const projects = ['project1', 'project2'];
+
+const todos = [
+  {
+    title: 'todo1',
+    dueDate: '2023-05-02',
+  },
+  {
+    title: 'todo2',
+    dueDate: '2023-06-03',
+  },
+];
+
+export { projects, todos };
