81b8d0746e70057dcd80eeda836a25ebb96395f3
Robinee <yb6925@naver.com>
2023-05-05 00:41:56 +0900

    Add parameter and removed unused variable

--- src/changeMain.js
+++ src/changeMain.js
@@ -2,9 +2,9 @@ import { data } from './data.js';
 import { main } from './component/main.js';
 import { getElement } from './getElement.js';
 
-function changeMain(e) {
+function changeMain(projectTitle) {
   //get clicked project title
-  const projectTitle = e.currentTarget.querySelector('.project-title');
+  //   const projectTitle = e.currentTarget.querySelector('.project-title');
   //find that project in data
   const index = data.findIndex((item) => {
     return item.project === projectTitle.textContent;
@@ -15,7 +15,6 @@ function changeMain(e) {
     const formattedMain = main(index);
     //get main to replace
     const currentMain = getElement('main');
-    console.log(formattedMain == currentMain);
     //load different main
     currentMain.replaceWith(formattedMain);
   } else if (index === -1) {
