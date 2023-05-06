5e91b972d6cb18ca832c0509e1805541f34cea17
Robinee <yb6925@naver.com>
2023-05-06 14:20:57 +0900

    Change name to 'loadInitialPage' because of confusion

---
+++ src/loadInitialPage.js
@@ -0,0 +1,16 @@
+import { header } from './component/header.js';
+import { sidebar } from './component/sidebar.js';
+import { main } from './component/main.js';
+import { controllDisplay } from './controllDisplay.js';
+// why added js at the end of file have an error?
+
+function loadInitialPage() {
+  const content = document.getElementById('content');
+  // load header, sidebar, main
+  const section = document.createElement('section');
+  section.append(sidebar, main(0));
+  content.append(header, section);
+  controllDisplay();
+}
+
+export { loadInitialPage };
