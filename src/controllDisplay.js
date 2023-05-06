ea98287908d3c087198bceccf6325ad8228e9cea
Robinee <yb6925@naver.com>
2023-05-06 16:48:45 +0900

    Add hideForm func and cancelBtn

--- src/controllDisplay.js
+++ src/controllDisplay.js
@@ -1,12 +1,19 @@
 function controllDisplay(main) {
   const addBtn = main.querySelector('.add-btn');
   const taskForm = main.querySelector('#task-form');
+  const cancelBtn = main.querySelector('.cancel');
+
   function displayForm() {
     taskForm.style.display = 'grid';
     addBtn.style.display = 'none';
   }
+  function hideForm() {
+    taskForm.style.display = 'none';
+    addBtn.style.display = 'block';
+  }
 
   addBtn.addEventListener('click', displayForm);
+  cancelBtn.addEventListener('click', hideForm);
 }
 
 export { controllDisplay };
