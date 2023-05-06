cc563fcbd351acd0452448f23fb535208c116201
Robinee <yb6925@naver.com>
2023-05-06 19:23:01 +0900

    change function name to new one controllForm

--- src/loadInitialPage.js
+++ src/loadInitialPage.js
@@ -2,7 +2,7 @@ import { header } from './component/header.js';
 import { sidebar } from './component/sidebar.js';
 import { main } from './component/main.js';
 // why added js at the end of file have an error?
-import { toggleEvents } from './controllFormContainer.js';
+import { controllForm } from './controllFormContainer.js';
 
 function loadInitialPage() {
   const content = document.getElementById('content');
@@ -11,7 +11,7 @@ function loadInitialPage() {
   const mainElement = main(0);
   section.append(sidebar, mainElement);
   content.append(header, section);
-  toggleEvents(mainElement);
+  controllForm(mainElement);
 }
 
 export { loadInitialPage };
