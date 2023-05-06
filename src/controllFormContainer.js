2dcc2aee4751100a2bbfbb9229455677894fc5f3
Robinee <yb6925@naver.com>
2023-05-06 19:21:58 +0900

    Change function name to controllForm

--- src/controllFormContainer.js
+++ src/controllFormContainer.js
@@ -1,4 +1,4 @@
-function toggleEvents(main, currentMain) {
+function controllForm(main, currentMain) {
   function displayForm() {
     taskForm.style.display = 'grid';
     addBtn.style.display = 'none';
@@ -31,35 +31,4 @@ function toggleEvents(main, currentMain) {
   cancelBtn.addEventListener('click', hideForm);
   taskForm.addEventListener('submit', test);
 }
-export { toggleEvents };
-
-// function controllDisplay(main, currentMain) {
-//     if (currentMain) {
-//       //remove eventListener from currentMain
-//       const addBtn = currentMain.querySelector('.add-btn');
-//       const cancelBtn = currentMain.querySelector('.cancel');
-
-//       addBtn.removeEventListener('click', displayForm);
-//       cancelBtn.removeEventListener('click', hideForm);
-//     }
-//     const addBtn = main.querySelector('.add-btn');
-//     const taskForm = main.querySelector('#task-form');
-//     const cancelBtn = main.querySelector('.cancel');
-
-//     function displayForm() {
-//       taskForm.style.display = 'grid';
-//       addBtn.style.display = 'none';
-//     }
-//     function hideForm() {
-//       taskForm.style.display = 'none';
-//       addBtn.style.display = 'block';
-//     }
-
-//     addBtn.addEventListener('click', displayForm);
-//     cancelBtn.addEventListener('click', hideForm);
-//     taskForm.addEventListener('submit', (e) => {
-//       e.preventDefault();
-//     });
-//   }
-
-//   export { controllDisplay };
+export { controllForm };
