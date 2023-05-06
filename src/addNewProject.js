76326a4b1737ed86c0d0c41bfbcc54c4820d2f8b
Robinee <yb6925@naver.com>
2023-05-06 14:11:28 +0900

    Save new added project in the data and make the code as two function showNewProject and saveNewProject

--- src/addNewProject.js
+++ src/addNewProject.js
@@ -1,25 +1,45 @@
 import { getElement } from './getElement.js';
+import { data } from './data.js';
+
+const dataCreator = (title) => {
+  return { project: title, todos: [] };
+};
 
 function addNewProject() {
   //add new project on the screen
+  const container = getElement('.projects-container');
   const formInput = getElement('.project-input');
   const projectName = formInput.value;
+
+  function showNewProject(title) {
+    //show new project on the screen
+    //make element using input.value
+    const element = document.createElement('li');
+    element.classList.add('project');
+    element.innerHTML = `<h2 class="project-title">${title}</h2>
+        <button class="remove-btn"><i class="fa-solid fa-xmark"></i></button>`;
+    //append in projects-container
+    container.append(element);
+  }
+
+  function saveNewProject(title) {
+    //save new project in data
+    //1. make data object structure of a new added project
+    const newData = dataCreator(title);
+    //2. push it to data
+    data.push(newData);
+    console.log(data);
+  }
+
   if (projectName.length === 0) {
     alert('Write down new project name');
     return;
   } else if (projectName.length > 0) {
-    //make element using input.value
-    const element = document.createElement('li');
-    element.classList.add('project');
-    element.innerHTML = `<h2 class="project-title">${projectName}</h2>
-        <button class="remove-btn"><i class="fa-solid fa-xmark"></i></button>`;
-    //append in projects-container
-    const container = getElement('.projects-container');
-    container.append(element);
+    saveNewProject(projectName);
+    showNewProject(projectName);
     // clean input value
     formInput.value = '';
   }
-  //save new project in data
 }
 
 export { addNewProject };
