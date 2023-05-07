d8277c794a3f140a07517893f169879c5f334864
Robinee <yb6925@naver.com>
2023-05-07 16:32:39 +0900

    Add Todo factory function

--- src/dataCreator.js
+++ src/dataCreator.js
@@ -1,5 +1,8 @@
 const Project = (title) => {
   return { title, todos: [] };
 };
+const Todo = (title, dueDate) => {
+  return { title, dueDate };
+};
 
-export { Project };
+export { Project, Todo };
