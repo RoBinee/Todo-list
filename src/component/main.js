76b0cd923fec2fa0dbc10adfc7914a5323e0c88e
Robinee <yb6925@naver.com>
2023-05-07 19:06:00 +0900

    Rewrite the code not using innerHTML to use formatTodoArticle func

--- src/component/main.js
+++ src/component/main.js
@@ -1,61 +1,54 @@
 import { data } from '../data.js';
+import { formatTodoArticle } from '../formatTodo.js';
+import { createElement } from '../getElement.js';
 
 const main = (index) => {
-  const main = document.createElement('main');
   const { title, todos } = data[index];
+  //create main element
+  const main = document.createElement('main');
 
-  function formatTodo(todos) {
-    //todos are array, so we need to use map func
-    const result = todos
-      .map((todo) => {
-        return `<article class="task">
-      <div>
-        <input type="checkbox" class="checkbox" />
-        <p class="task-title">${todo.title}</p>
-      </div>
-      <div>
-        <p class="task-dueDate">${todo.dueDate}</p>
-        <button class="task-btn edit">
-          <i class="fa-solid fa-pen"></i>
-        </button>
-        <button class="task-btn delete">
-          <i class="fa-solid fa-trash"></i>
-        </button>
-      </div>
-    </article>`;
-      })
-      .join(' ');
-    return result;
-  }
+  //create container
+  const container = createElement('div', 'container');
 
-  main.innerHTML = `<div class="container">
-          <h2 class="main-title">${title}</h2>
-          <div class="task-container">
-          ${formatTodo(todos)}
-          </div>
-          <div class="btn-form-container">
-            <button class="add-btn">+ Add Task</button>
-            <form action="" id="task-form" novalidate>
-              <input
-                type="text"
-                class="task-form-input"
-                autocomplete="off"
-                id="title"
-                placeholder="title"
-                required
-              />
-              <select name="" id="priority" class="task-form-input">
-                <option value="urgent" selected>urgent</option>
-                <option value="not urgent">not urgent</option>
-              </select>
-              <input type="date" id="dueDate" class="task-form-input" />
-              <div class="btn-container">
-                <button type="button" class="task-form-btn cancel">Cancel</button>
-                <button type="submit" class="task-form-btn add">Add</button>
-              </div>
-            </form>
-          </div>
-        </div>`;
+  //create h2 main-title
+  const mainTitle = createElement('h2', 'main-title');
+  mainTitle.textContent = title;
+
+  //create div task-container
+  const taskContainer = createElement('div', 'task-container');
+  //todoElement is an array. add each element into taskContainer
+  const todoElements = todos.map((todo) => formatTodoArticle(todo));
+  todoElements.forEach((todo) => {
+    taskContainer.append(todo);
+  });
+
+  //create div form-container
+  const formContainer = createElement('div', 'form-container');
+  formContainer.innerHTML = `<button class="add-btn">+ Add Task</button>
+  <form action="" id="task-form" novalidate>
+    <input
+      type="text"
+      class="task-form-input"
+      autocomplete="off"
+      id="title"
+      placeholder="title"
+      required
+    />
+    <select name="" id="priority" class="task-form-input">
+      <option value="urgent" selected>urgent</option>
+      <option value="not urgent">not urgent</option>
+    </select>
+    <input type="date" id="dueDate" class="task-form-input" />
+    <div class="btn-container">
+      <button type="button" class="task-form-btn cancel">Cancel</button>
+      <button type="submit" class="task-form-btn add">Add</button>
+    </div>
+  </form>`;
+
+  //append main-title, task-container, form-container into container
+  container.append(mainTitle, taskContainer, formContainer);
+
+  main.append(container);
 
   return main;
 };
