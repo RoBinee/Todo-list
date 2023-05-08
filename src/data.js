6a169cfbbd18d4160b9e5829175218f36436a1af
Robinee <yb6925@naver.com>
2023-05-09 00:09:41 +0900

    Remake data as obj with setter and getter

--- src/data.js
+++ src/data.js
@@ -1,30 +1,67 @@
-const data = [
-  {
-    title: 'One project',
-    todos: [
-      {
-        title: 'One project todo',
-        dueDate: '2027-12-02',
-      },
-      {
-        title: 'One project todo2',
-        dueDate: '2023-06-03',
-      },
-    ],
-  },
-  {
-    title: 'Two project',
-    todos: [
-      {
-        title: 'Two project todo',
-        dueDate: '2027-12-02',
-      },
-      {
-        title: 'Two project todo2',
-        dueDate: '2023-06-03',
-      },
-    ],
-  },
-];
+// const data = [
+//   {
+//     title: 'One project',
+//     todos: [
+//       {
+//         title: 'One project todo',
+//         dueDate: '2027-12-02',
+//       },
+//       {
+//         title: 'One project todo2',
+//         dueDate: '2023-06-03',
+//       },
+//     ],
+//   },
+//   {
+//     title: 'Two project',
+//     todos: [
+//       {
+//         title: 'Two project todo',
+//         dueDate: '2027-12-02',
+//       },
+//       {
+//         title: 'Two project todo2',
+//         dueDate: '2023-06-03',
+//       },
+//     ],
+//   },
+// ];
 
-export { data };
+let obj = {
+  data: [
+    {
+      title: 'One project',
+      todos: [
+        {
+          title: 'One project todo',
+          dueDate: '2027-12-02',
+        },
+        {
+          title: 'One project todo2',
+          dueDate: '2023-06-03',
+        },
+      ],
+    },
+    {
+      title: 'Two project',
+      todos: [
+        {
+          title: 'Two project todo',
+          dueDate: '2027-12-02',
+        },
+        {
+          title: 'Two project todo2',
+          dueDate: '2023-06-03',
+        },
+      ],
+    },
+  ],
+  get projects() {
+    return this.data;
+  },
+  set projects(array) {
+    this.data = array;
+  },
+};
+
+export { obj };
